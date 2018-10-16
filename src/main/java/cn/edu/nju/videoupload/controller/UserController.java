b484f58bdb1c204b5256ddb8c72a139e333a615b
lyb <734429286@qq.com>
2018-10-16 18:15:31 +0800

    2

---
+++ src/main/java/cn/edu/nju/videoupload/controller/UserController.java
@@ -0,0 +1,26 @@
+package cn.edu.nju.videoupload.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@EnableAutoConfiguration
+
+public class UserController {
+    @RequestMapping("/tryLogin")
+    @ResponseBody
+    String home() {
+        return "Hello ,spring boot!";
+    }
+    @RequestMapping("/")
+    public ModelAndView index(){
+        return new ModelAndView("blanke.html");
+
+
+
+    }
+
+}

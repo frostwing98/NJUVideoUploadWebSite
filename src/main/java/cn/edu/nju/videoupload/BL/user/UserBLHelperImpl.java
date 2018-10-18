bb479d09c60183373881008ba5365139717cc1c2
Kodomo <260386109@qq.com>
2018-10-18 12:31:33 +0800

    UserBLHelper接口补充, UserBLHelperImpl部分实现, UserBLServiceImpl部分实现

--- src/main/java/cn/edu/nju/videoupload/BL/user/UserBLHelperImpl.java
+++ src/main/java/cn/edu/nju/videoupload/BL/user/UserBLHelperImpl.java
@@ -2,17 +2,21 @@ package cn.edu.nju.videoupload.BL.user;
 
 import cn.edu.nju.videoupload.Dao.UserDao;
 import cn.edu.nju.videoupload.entity.User;
+import cn.edu.nju.videoupload.entity.UserVO;
 import cn.edu.nju.videoupload.utils.BadUserFormatException;
+import cn.edu.nju.videoupload.utils.BadUserVOFormatException;
 import cn.edu.nju.videoupload.utils.UserNotExistException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
 
 /**
  * @author lzb
  * @date 2018/10/16 19:22
  */
 @Service
-public class UserBLHelperImpl implements UserBLHelper{
+public class UserBLHelperImpl implements UserBLHelper {
 
     private final UserDao userDao;
 
@@ -44,5 +48,34 @@ public class UserBLHelperImpl implements UserBLHelper{
     @Override
     public boolean ifUserIDExists(String userID) throws UserNotExistException {
         return false;
+    }
+
+    @Override
+    public UserVO userToVo(User user) {
+        UserVO vo = new UserVO();
+        vo.setUserID(user.getUserID());
+        vo.setPassword(user.getPassword());
+        if (user.getPropertyMap() != null) {
+            vo.setPropertyMap(new HashMap<>(user.getPropertyMap()));
+        } else {
+            vo.setPropertyMap(new HashMap<>());
+        }
+        return vo;
+    }
+
+    @Override
+    public User voToUser(UserVO vo) throws BadUserVOFormatException {
+        if (vo.getPassword() == null || vo.getUserID() == null) {
+            throw new BadUserVOFormatException("缺少用户名或密码");
+        }
+        User user = new User();
+        user.setUserID(vo.getUserID());
+        user.setPassword(vo.getPassword());
+        if (vo.getPropertyMap() == null) {
+            user.setPropertyMap(new HashMap<>());
+        } else {
+            user.setPropertyMap(new HashMap<>(vo.getPropertyMap()));
+        }
+        return user;
     }
 }
--- src/main/java/cn/edu/nju/videoupload/BL/user/UserBLServiceImpl.java
+++ src/main/java/cn/edu/nju/videoupload/BL/user/UserBLServiceImpl.java
@@ -1,6 +1,7 @@
 package cn.edu.nju.videoupload.BL.user;
 
 import cn.edu.nju.videoupload.BLService.UserBLService;
+import cn.edu.nju.videoupload.entity.User;
 import cn.edu.nju.videoupload.entity.UserVO;
 import cn.edu.nju.videoupload.utils.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,14 +26,39 @@ public class UserBLServiceImpl implements UserBLService {
         this.excelHelper = excelHelper;
     }
 
+    /**
+     * 用于从Excel文件中导入数据，
+     *
+     * @param excelFile excel文件的MultipartFile
+     * @throws BadExcelFileException Excel文件的格式不符合要求
+     */
     @Override
     public void importUsersByExcel(MultipartFile excelFile) throws BadExcelFileException {
 
     }
 
+    /**
+     * 用于普通用户登录
+     *
+     * @param userID   用户ID
+     * @param password 用户密码
+     * @return UserVO
+     * @throws LogInFailedException 登录失败
+     */
     @Override
     public UserVO login(String userID, String password) throws LogInFailedException {
-        return null;
+        try {
+            User user = userBLHelper.getUserByID(userID);
+            if (user.getPassword().equals(password)) {
+                //登陆成功
+                return userBLHelper.userToVo(user);
+            } else {
+                //登录失败
+                throw new LogInFailedException("用户名或密码错误");
+            }
+        } catch (UserNotExistException e) {
+            throw new LogInFailedException("用户不存在");
+        }
     }
 
     /**
@@ -44,7 +70,8 @@ public class UserBLServiceImpl implements UserBLService {
      */
     @Override
     public UserVO fetchUserInfo(String userID) throws UserNotExistException {
-        return null;
+        User user = userBLHelper.getUserByID(userID);
+        return userBLHelper.userToVo(user);
     }
 
     /**
@@ -57,7 +84,9 @@ public class UserBLServiceImpl implements UserBLService {
      */
     @Override
     public UserVO updateUseInfo(UserVO newUserVO) throws UserNotExistException, BadUserVOFormatException {
-        return null;
+        User user = userBLHelper.voToUser(newUserVO);
+        userBLHelper.updateUser(user);
+        return fetchUserInfo(user.getUserID());
     }
 
     @Override

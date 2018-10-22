714da4e287215eeb0ab3f220703092714a4bbcf4
frostwing98 <498924217@qq.com>
2018-10-22 23:03:05 +0800

    update myvideoupdate method

--- src/main/java/cn/edu/nju/videoupload/MyVideouploadApplication.java
+++ src/main/java/cn/edu/nju/videoupload/MyVideouploadApplication.java
@@ -1,12 +1,16 @@
 package cn.edu.nju.videoupload;
 
+import cn.edu.nju.videoupload.businesslogic.VideoBL;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VideouploadApplication {
+public class MyVideouploadApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VideouploadApplication.class, args);
+		VideoBL videoBL=new VideoBL();
+		videoBL.upload();
+
+		SpringApplication.run(MyVideouploadApplication.class, args);
 	}
 }
---
+++ src/main/java/cn/edu/nju/videoupload/entity/Message.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.videoupload.entity;
+
+/**
+ * @Author: Yang Yuqing
+ * @Description:
+ * @Date: Created in 21:08 14.10.18
+ * @Modifiedby:
+ */
+
+public class Message {
+    public static final int SUCCESS = 200;
+    public static final int GENERAL_FAIL = 404;
+}
